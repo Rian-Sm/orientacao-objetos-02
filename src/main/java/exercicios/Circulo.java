13a835516022544d6781c80353bd9acad4e30525
rian santos <riansm100@gmail.com>
2020-03-05 13:07:09 -0300

    criado a classe circulo

---
+++ src/main/java/exercicios/Circulo.java
@@ -0,0 +1,15 @@
+package exercicios;
+
+public class Circulo {
+    private Double raio;
+
+    public void setRaio(Double raio){
+        this.raio = raio;
+    }
+    public Double CalcularArea(){
+        return Math.PI * Math.pow(raio, 2);
+    }
+    public Double calcularPerimetro(){
+        return 2 * Math.PI * raio;
+    }
+}
