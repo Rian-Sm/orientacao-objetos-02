d5469da5ae78423bdf9b1ca2e82902706b5831c9
rian santos <riansm100@gmail.com>
2020-03-11 05:47:32 -0300

    correção de erro de ponteiro nulo

--- src/main/java/exercicios/Correntista.java
+++ src/main/java/exercicios/Correntista.java
@@ -2,26 +2,29 @@ package exercicios;
 
 public class Correntista {
     private Integer numero;
-    private Double saldo;
+    private Double saldo= 0.0;
 
     public Double getSaldo(){
         return saldo;
     }
-
+    public Integer getNumero(){
+        return numero;
+    }
     public void setNumero(Integer numero){
         this.numero = numero;
     }
     public void depositar(Double valor){
-        if (valor >=0)
-            throw new IllegalArgumentException("Depósito invalido");
-        saldo = saldo + valor;
+        if (valor<=0)
+            throw new IllegalArgumentException("falha de processo");
+        saldo += valor;
     }
     public void sacar(Double valor){
-        if(valor > saldo && valor <= 0)
-            throw new IllegalArgumentException("Saque Invalido");
-        saldo = valor - 1;
+        if (valor<=0 || valor>saldo )
+            throw new IllegalArgumentException("falha de processo");
+        saldo -= valor;
     }
     public void transferir(Correntista correntista, Double valor){
-
+        sacar(valor);
+        correntista.depositar(valor);
     }
 }
