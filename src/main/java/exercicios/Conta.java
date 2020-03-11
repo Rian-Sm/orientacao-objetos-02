b4bbd336784b7164564bfdf77f7f44accac808ac
rian santos <riansm100@gmail.com>
2020-03-11 06:22:51 -0300

    criado a classe conta

---
+++ src/main/java/exercicios/Conta.java
@@ -0,0 +1,20 @@
+package exercicios;
+
+public class Conta {
+    private Integer numero;
+    private Double saldo;
+    private Double  limite;
+
+    public Double getSaldo(){
+        return saldo;
+    }
+    public void depositar(Double valor){
+
+    }
+    public void sacar(){
+
+    }
+    public void transferir(){
+        
+    }
+}
